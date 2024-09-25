669869fd5bc257d5e16191c35c4a176be8e22412
Eyrian Muet <eyrianmuet@gmail.com>
2024-09-25 13:48:04 +1100

    Ajout de la gestion de la route random/:nb

--- server-http.mjs
+++ server-http.mjs
@@ -10,13 +10,25 @@ async function requestListener(request, response) {
     response.setHeader("Content-Type", "text/html");
     try {
         const contents = await fs.readFile("index.html", "utf8");
-        switch (request.url) {
-            case "/index.html":
+        console.log(request.url.split("/")[1]);
+        switch (request.url.split("/")[1]) {
+            case "":
+            case "index.html":
                 response.writeHead(200);
                 return response.end(contents);
-            case "/random.html":
+            case "random.html":
                 response.writeHead(200);
                 return response.end(`<html><p>${Math.floor(100 * Math.random())}</p></html>`);
+            case "random":
+                response.writeHead(200);
+                console.log(request.url.split("/"));
+                console.log(request.url.split("/").pop());
+                const nb = parseInt(request.url.split("/").pop());
+                let text = "";
+                for (let i = 0; i < nb; i++) {
+                    text += `<p>${Math.floor(100 * Math.random())}</p>`;
+                }
+                return response.end(`<html><p>${text}</p></html>`);
             default:
                 response.writeHead(404);
                 return response.end(`<html><p>404: NOT FOUND</p></html>`);
